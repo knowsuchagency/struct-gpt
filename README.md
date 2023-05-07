805dc5d8e5f28902e624f1dec194ae609b402bcc
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2023-05-07 01:09:57 -0700

    published

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
 # struct-gpt
 
 structured llm outputs
+
+## Usage
+
+```python
+from struct_gpt import OpenAiBase
+from pydantic import Field
+
+class SentimentSchema(OpenAiBase):
+    """
+    Determine the sentiment of the given text:
+
+    {content}
+    """
+
+    sentiment: str = Field(description="Either -1, 0, or 1.")
+
+positive = SentimentSchema.create(content="I love pizza!")
+print(f"{positive = }") # positive = SentimentSchema(sentiment='1')
+```
+
+```python
+class SentimentAnalysis(OpenAiBase):
+    """
+    Determine the sentiment of each word in the following: {text}
+    """
+
+    sentiment: Mapping[str, SentimentSchema]
+
+
+analysis = SentimentAnalysis.create(text="I love the beautiful scenery, but the long hike was exhausting.")
+print(analysis.json(indent=2))
+```
+outputs:
+```json
+{
+  "sentiment": {
+    "I": {
+      "sentiment": "1"
+    },
+    "love": {
+      "sentiment": "1"
+    },
+    "the": {
+      "sentiment": "0"
+    },
+    "beautiful": {
+      "sentiment": "1"
+    },
+    "scenery,": {
+      "sentiment": "1"
+    },
+    "but": {
+      "sentiment": "0"
+    },
+    "long": {
+      "sentiment": "-1"
+    },
+    "hike": {
+      "sentiment": "-1"
+    },
+    "was": {
+      "sentiment": "0"
+    },
+    "exhausting.": {
+      "sentiment": "-1"
+    }
+  }
+}
+```
--- struct_gpt/models.py
+++ struct_gpt/models.py
@@ -1,16 +1,18 @@
 import io
 import json
-from pprint import pprint
+import logging
 from typing import TypeVar, Literal, Mapping
 
 import openai
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError
 from ruamel.yaml import YAML
 
 Model = TypeVar("Model", bound=BaseModel)
 Example = Mapping[Literal["input", "output"], str]
 
 yaml = YAML(typ="safe")
+
+logger = logging.getLogger(__name__)
 
 
 class OpenAiMixin:
@@ -23,73 +25,94 @@ class OpenAiMixin:
         examples: list[Example] = None,
         **kwargs,
     ) -> Model:
+        """
+        Create a new model instance using OpenAI's API.
+
+        Args:
+            temperature: Controls the randomness of the response.
+            model: The specific OpenAI model to use.
+            retries: Number of times to retry in case of failure.
+            examples: List of input-output example pairs. Useful to improve the model's accuracy.
+            **kwargs: Used to format the model's docstring.
+
+        Returns:
+            Model instance.
+        """
         assert (
-            doc := cls.__doc__
+            doc := cls.__doc__.strip()
         ), "please add a docstring explaining how to destructure the prompt"
 
+        assert (
+            examples or kwargs
+        ), "please provide either examples or keyword args for the docstring"
+
+        # Convert the JSON schema to YAML since it takes up fewer tokens
         with io.StringIO() as fp:
             json_schema = json.loads(cls.schema_json())
             yaml.dump(json_schema, fp)
             yaml_json_schema = fp.getvalue()
 
+        # Set up the system message to guide the assistant
         system_message = {
             "role": "system",
-            "content": f"Please respond ONLY with valid YAML that conforms to this pydantic json_schema: {yaml_json_schema}. Output will be deserialized with {cls.__name__}.parse_obj(yaml.load(content)).",
+            "content": f"Please respond ONLY with valid json that conforms to this json_schema: {yaml_json_schema}. Don't include additional text other than the object as it will be deserialized with pydantic_model.parse_raw",
         }
 
         messages = [system_message]
 
+        # Add examples to the messages if provided
         if examples:
             for example in examples:
                 messages.append({"role": "user", "content": example["input"]})
                 messages.append({"role": "assistant", "content": example["output"]})
-        else:
-            messages.append(
-                {
-                    "role": "user",
-                    "content": doc.format(**kwargs),
-                }
-            )
 
-        response = openai.ChatCompletion.create(
-            messages=messages,
-            temperature=temperature,
-            model=model,
+        prompt = doc.format(**kwargs)
+
+        messages.append(
+            {
+                "role": "user",
+                "content": prompt,
+            }
         )
-        response_content = response["choices"][0]["message"]["content"]
-        return cls.parse_obj(yaml.load(response_content))
 
         attempt = 0
+        last_exception = None
 
-        while (
-            response := openai.ChatCompletion.create(
-                messages=messages,
-                temperature=temperature,
-                model=model,
-            )
-            and attempt < retries
-        ):
-            pprint(response)
-
-            response_content = response["choices"][0]["message"]["content"]
+        # Retry the specified number of times in case of failure
+        while attempt < retries:
+            try:
+                content = openai.ChatCompletion.create(
+                    messages=messages,
+                    temperature=temperature,
+                    model=model,
+                )["choices"][0]["message"]["content"]
+                obj = json.loads(content)
+            except Exception as e:
+                last_exception = e
+                error_msg = f"json.loads error: {e}"
+                logger.error(error_msg)
+                messages.append(
+                    {"role": "user", "content": f"{e.__class__.__name__}: {e}"}
+                )
+                attempt += 1
+                continue
 
             try:
-                return cls.parse_obj(yaml.load(response_content))
-            except Exception as e:
+                return cls.parse_obj(obj)
+            except ValidationError as e:
+                last_exception = e
+                error_msg = f"pydantic.ValidationError: {e}"
+                logger.error(error_msg)
                 messages.append(
                     {"role": "user", "content": f"{e.__class__.__name__}: {e}"}
                 )
                 attempt += 1
 
-
-class YamlMixin:
-    def yaml(self: Model):
-        with io.StringIO() as fp:
-            yaml.dump(self.dict(), fp)
-            return fp.getvalue()
+        if last_exception:
+            raise last_exception
 
 
-class OpenAiBase(BaseModel, OpenAiMixin, YamlMixin):
+class OpenAiBase(BaseModel, OpenAiMixin):
     ...
 
 
@@ -105,13 +128,18 @@ if __name__ == "__main__":
         sentiment: str = Field(description="Either -1, 0, or 1.")
 
     examples = [
-        {"input": "I love pizza!", "output": SentimentSchema(sentiment="1").yaml()},
-        {"input": "I hate pizza!", "output": SentimentSchema(sentiment="-1").yaml()},
+        {
+            "input": "this library is neat!",
+            "output": SentimentSchema(sentiment="1").json(),
+        },
+        {
+            "input": "don't touch that",
+            "output": SentimentSchema(sentiment="-1").json(),
+        },
     ]
 
     positive = SentimentSchema.create(
         content="I love pizza!",
-        # examples=examples,
     )
     negative = SentimentSchema.create(
         content="I hate pizza!",
